4949813ad1ae061eb0f8e48b35be0aadd28b1ac4
Renbin <rbliu@mit.edu>
2020-05-26 16:08:32 -0700

    add in database URL config

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import pdb
 import sys
+import os
+import psycopg2
+from dotenv import load_dotenv
+
 from .werewolf_roles import roles
 
 app = Flask(__name__)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 @app.route('/')
 def render_HTML():
